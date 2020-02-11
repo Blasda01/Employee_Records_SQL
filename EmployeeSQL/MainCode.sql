36a6befc78331e39bcee4228167de12691a9a305
David Blascyk <davidblascyk@gmail.com>
2020-02-11 09:50:26 -0600

    added readme and housekeeping

---
+++ EmployeeSQL/MainCode.sql
@@ -0,0 +1,57 @@
+--Returns employee no, name, gender and salary
+SELECT e.emp_no, e.first_name, e.last_name, e.gender, s.salary
+FROM employee e
+JOIN salaries s
+ON e.emp_no = s.emp_no;
+
+--Employees hired in 1986
+SELECT e.emp_no, e.first_name, e.last_name, e.hire_date
+FROM employee e
+WHERE e.hire_date SIMILAR TO '1986%';
+
+--List manager information for each department
+SELECT man.dept_no, dep.dept_name, e.emp_no, e.last_name, e.first_name, man.from_date, man.to_date
+FROM dept_manager man
+JOIN departments dep
+ON man.dept_no = dep.dept_no
+JOIN employee e
+ON man.emp_no = e.emp_no;
+
+--List department of each employee
+SELECT e.emp_no, e.last_name, e.first_name, dep.dept_name
+FROM employee e
+JOIN dept_employee demp
+ON e.emp_no = demp.emp_no
+JOIN departments dep
+ON demp.dept_no = dep.dept_no;
+
+--List of employees with name Hercules B.
+SELECT e.emp_no, e.first_name, e.last_name
+FROM employee e
+WHERE e.first_name ILIKE 'Hercules'
+AND e.last_name SIMILAR TO 'B%';
+
+--List all employees in Sales Dept
+SELECT e.emp_no, e.last_name, e.first_name, dep.dept_name
+FROM dept_employee demp
+JOIN departments dep
+ON demp.dept_no = dep.dept_no
+JOIN employee e
+ON demp.emp_no = e.emp_no
+WHERE dep.dept_name LIKE 'Sales';
+
+--List all employees in Sales and Development depts
+SELECT e.emp_no, e.last_name, e.first_name, dep.dept_name
+FROM dept_employee demp
+JOIN departments dep
+ON demp.dept_no = dep.dept_no
+JOIN employee e
+ON demp.emp_no = e.emp_no
+WHERE dep.dept_name LIKE 'Sales'
+OR dep.dept_name LIKE 'Development';
+
+--List frequency count of employee last name
+SELECT last_name, COUNT(last_name) as Last_name_freq
+FROM employee
+GROUP BY last_name 
+ORDER BY COUNT(last_name) DESC;
--- README.md
+++ README.md
@@ -1,1 +1,44 @@
-# sql-challenge
+**# Employee_Records_SQL**
+
+For this project I created the tables to hold data in the CSVs, imported the CSVs into a SQL database, and created numerous queries on the data. In other words:
+
+1. Data Modeling
+
+2. Data Engineering
+
+3. Data Analysis
+
+
+#### Data Modeling
+
+Inspect the CSVs and sketch out an ERD of the tables. Using [http://www.quickdatabasediagrams.com](http://www.quickdatabasediagrams.com).
+
+#### Data Engineering
+
+* Create a table schema for each of the six CSV files. Including specific data types, primary keys, foreign keys, and other constraints.
+
+* Import each CSV file into the corresponding SQL table.
+
+#### Data Analysis
+
+With a complete database, I designed mulitple SQL queries of the data (see MainCode.sql)
+
+Then imported data into a Pandas DataFrame using Jupyter Notebook to create visual of Average Salaries.
+
+**Tools**
+
+--ERD design with [http://www.quickdatabasediagrams.com](http://www.quickdatabasediagrams.com)
+--SQL and PostgreSQL database
+--Python Libraries: Pandas, sqlalchemy
+
+
+**ERD Image and Analysis BarChart**
+
+![Graph 1]("images/ERD.PNG")
+
+
+![Graph 2]("images/salary.png")
+
+
+
+
